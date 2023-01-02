d6d2ac2a8ce059dcd4e470005dc4da4d2eceba7e
TheInfamousGrim <finchergeorge1@gmail.com>
2023-01-02 04:53:30 +0000

    Feat: complete reverse string exercise
    Completed the reverse string exercise showing that I can reverse a
    string without using high order array methods.

---
+++ 01-arrays-strings/01-two-pointers/reverse-string.js
@@ -0,0 +1,48 @@
+/* 
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 105
+    s[i] is a printable ascii character.
+
+*/
+
+const inputOne = ["h","e","l","l","o"];
+const inputTwo = ["H","a","n","n","a","h"];
+
+const reverseString = (stringArray) => {
+    let left = 0;
+    let right = stringArray.length - 1;
+    
+    while (left < right) {
+        // swap the values
+        let initialLeft = stringArray[left];
+        let initialRight = stringArray[right];
+        stringArray[left] = initialRight;
+        stringArray[right] = initialLeft;
+        // Increment the left pointer
+        left++;
+        // Decrement the right pointer
+        right--;
+    }
+    
+    return stringArray; 
+}
+
+console.log(reverseString(inputOne));
+console.log(reverseString(inputTwo));
